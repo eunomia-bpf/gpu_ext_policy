6834871a374463a10f9fbf318cf8b2047c139479
Littlefisher <i@littlefisher.me>
2025-11-23 17:36:11 -0800

    Define uvm_gpu_chunk_struct for BPF access to chunk list field

--- src/uvm_types.h
+++ src/uvm_types.h
@@ -26,8 +26,30 @@ typedef struct uvm_perf_prefetch_bitmap_tree {
 
 /* Forward declarations - opaque types for BPF */
 typedef struct uvm_pmm_gpu_struct uvm_pmm_gpu_t;
-typedef struct uvm_gpu_chunk_struct uvm_gpu_chunk_t;
 typedef struct uvm_va_block_struct uvm_va_block_t;
+
+/* Full definition of uvm_gpu_chunk_struct - needed to access chunk->list field */
+struct uvm_gpu_chunk_struct {
+	unsigned long long address;
+	/* We only need the list field for BPF, but include minimal structure */
+	struct {
+		unsigned int type : 2;
+		unsigned int in_eviction : 1;
+		unsigned int inject_split_error : 1;
+		unsigned int is_zero : 1;
+		unsigned int is_referenced : 1;
+		unsigned int state : 3;
+		unsigned int log2_size : 6;
+		unsigned short va_block_page_index : 10;
+		unsigned int gpu_index : 7;
+	};
+	struct list_head list;  /* This is what we need to access */
+	void *va_block;
+	void *parent;
+	void *suballoc;
+};
+
+typedef struct uvm_gpu_chunk_struct uvm_gpu_chunk_t;
 
 /* Note: list_head is already defined in vmlinux.h, no need to redefine it */
 
